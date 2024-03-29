5ee9d012e6d0dad163e9ae09309d4ba22fbe37a8
Mex <daniel.mexcze@gmail.com>
2024-03-29 10:21:09 +0100

    asdf

---
+++ budik/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TimeCalculator from './components/TimeCalculator'
+
+const App = () => {
+  return (
+    <div>
+      <TimeCalculator />
+    </div>
+  )
+}
+
+export default App
